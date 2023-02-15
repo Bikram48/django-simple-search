2cb888f290188cab90d3dc4cdb16ed0add4024aa
Bikram48 <chandbikram001@gmail.com>
2023-02-15 14:35:52 +0545

    add - models for blogs

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Blogs(models.Model):
+    title = models.TextField()
+    author = models.TextField()
+    content = models.TextField()
