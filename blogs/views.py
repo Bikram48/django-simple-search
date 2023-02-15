e2a0a38be1630bfd461e67ab0155e609201fb3ea
Bikram48 <chandbikram001@gmail.com>
2023-02-15 15:05:07 +0545

    show - search result

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from blogs.models import Blogs
 
 # Create your views here.
 def search_view(request):
-    context = {}
+    query = request.GET
+
+    try:
+        query_obj = int(query.get("q"))
+    except:
+        query_obj = None
+
+    print( query )
+    
+    blog_objects = None
+    if query_obj is not None:
+        blog_objects = Blogs.objects.get(id=query_obj)
+        print(blog_objects)
+    context = {
+        "object": blog_objects
+    }
     return render(request, "search.html", context=context)
